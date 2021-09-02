53cd18e981f249b3f27eeb8f164e0353e8257bc7
11t518s <11t518s20161759@gmail.com>
2021-09-02 17:57:55 +0900

    순서쌍으로 작업 중

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ const solution = () => {
 const numbers = [1,2,3,4,5,6,7,8,9,0];
 const hand = 'right'
     let answer = '';
-    let positionOfLeft = 3;
-    let positionOfRight = 3;
+    let positionOfLeft = [0,3];
+    let positionOfRight = [2,3];
     for (let i = 0; i < numbers.length; i ++) {
         console.log(positionOfLeft)
         if (numbers[i] === 1 || numbers[i] === 4 ||numbers[i] === 7) {
             answer = answer + "L";
-            positionOfLeft = parseInt(numbers[i] / 3)
+            positionOfLeft = [numbers[i]%3,parseInt(numbers[i] / 3)]
         } else if (numbers[i] === 3 || numbers[i] ===6 || numbers[i] ===9) {
             answer = answer + "R";
-            positionOfRight = parseInt(numbers[i] /3) -1
+            positionOfRight = [numbers[i]%3,parseInt(numbers[i] / 3)-1]
         } else {
-            let givenNumber = numbers[i] / 3
+            let givenNumber = [numbers[i]%3,parseInt(numbers[i] / 3)]
             if (Math.abs(givenNumber - positionOfLeft )> Math.abs(givenNumber - positionOfRight)) {
                 answer = answer + "R";
             } else if (Math.abs(givenNumber - positionOfLeft )< Math.abs(givenNumber - positionOfRight)) {
