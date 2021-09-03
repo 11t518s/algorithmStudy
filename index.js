2390c7d932d3c30019d6216d08c964f34cd3c42d
11t518s <11t518s20161759@gmail.com>
2021-09-03 16:27:44 +0900

    키패드 누르기: 성공, 그러나 복잡도가 너무 커서 줄여야함

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const test = document.getElementById('target');
 
 test.innerText = '일단 이거는 잘 되고'
 const solution = () => {
-const numbers = [1,2,3,4,5,6,7,8,9,0];
-const hand = 'right'
+
     let answer = '';
     let positionOfLeft = [0,3];
     let positionOfRight = [2,3];
@@ -18,18 +17,20 @@ const hand = 'right'
             positionOfRight = [numbers[i]%3,parseInt(numbers[i] / 3)-1]
         } else {
             let givenNumber = [numbers[i]%3,parseInt(numbers[i] / 3)]
-            if (Math.abs(givenNumber - positionOfLeft )> Math.abs(givenNumber - positionOfRight)) {
+            if (Math.abs(givenNumber[0] - positionOfLeft[0] ) +Math.abs(givenNumber[1] - positionOfLeft[1] )  > Math.abs(givenNumber[0] - positionOfRight[0] ) + Math.abs(givenNumber[1] - positionOfRight[1] )) {
                 answer = answer + "R";
-            } else if (Math.abs(givenNumber - positionOfLeft )< Math.abs(givenNumber - positionOfRight)) {
+                positionOfRight = givenNumber
+            } else if (Math.abs(givenNumber[0] - positionOfLeft[0] ) +Math.abs(givenNumber[1] - positionOfLeft[1] )  > Math.abs(givenNumber[0] - positionOfRight[0] ) + Math.abs(givenNumber[1] - positionOfRight[1] )) {
                 answer = answer + "L";
+                positionOfLeft = givenNumber
+
             } else {
                 answer = answer+ hand[0].toUpperCase()
             }
 
         }
     }
-    console.log(positionOfLeft)
-    console.log(answer)
+
     return answer;
 }
 test.addEventListener('click', solution)
