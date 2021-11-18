a63e82a2197536fd065b5560fb520fe5669248da
11t518s <11t518s20161759@gmail.com>
2021-11-19 08:17:45 +0900

    programmers k번째 수

---
+++ programmersNumberOf'K'th.js
@@ -0,0 +1,7 @@
+function solution(array, commands) {
+  var answer = [];
+  commands.map((item) => {
+    answer.push(array.slice(item[0] - 1, item[1]).sort((a, b) => a - b)[item[2] - 1]);
+  });
+  return answer;
+}
